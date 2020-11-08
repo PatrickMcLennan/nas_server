e31c45022623f3b382e1488f1620bddb906f9579
Patrick McLennan <patrick.a.mclennan@gmail.com>
2020-11-08 18:19:14 -0500

    Move metadata work

--- index.ts
+++ index.ts
@@ -1,20 +1,13 @@
 import express from 'express';
-import { networkInterfaces } from 'os';
 import { typeDefs } from './graphql/typedefs';
 import { resolvers } from './graphql/resolvers';
 import { ApolloServer } from 'apollo-server-express';
 
-const ip =
-  networkInterfaces()?.en0?.find(
-    ({ family }) => family.toLowerCase() === `ipv4`
-  ).address ?? `Problem detecting IP`;
 const port = 4000;
-
-console.log(ip);
 
 const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.applyMiddleware({ app, path: `/api` });
 // Start the server
-app.listen(port, () => console.log(`Running on ${ip}:${port}`));
+app.listen(port, () => console.log(`Running on Port ${port}`));
