0a48cd1b26378377ee5e7e2f371f7176019e2d87
Patrick McLennan <patrick.a.mclennan@gmail.com>
2020-11-08 14:41:26 -0500

    Verbiage tweaks

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
+import { networkInterfaces } from 'os';
 import { typeDefs } from './graphql/typedefs';
 import { resolvers } from './graphql/resolvers';
 import { ApolloServer } from 'apollo-server-express';
+
+const ip =
+  networkInterfaces()?.en0?.find(
+    ({ family }) => family.toLowerCase() === `ipv4`
+  ).address ?? `Problem detecting IP`;
+const port = 4000;
+
+console.log(ip);
 
 const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.applyMiddleware({ app, path: `/api` });
 // Start the server
-app.listen(4000, () => {
-  console.log('Go to http://localhost:4000/graphiql to run queries!');
-});
+app.listen(port, () => console.log(`Running on ${ip}:${port}`));
