6530b2efc14230762bf5a99b4855943edfd84c32
Patrick McLennan <patrick.a.mclennan@gmail.com>
2020-12-06 19:06:37 -0500

    movie metadata work

--- metadata/movies.ts
+++ metadata/movies.ts
@@ -11,7 +11,13 @@ config({ path: path.resolve(__dirname, `../../.env`) });
  * Scan the directory, pick up on new ID's, make an entry for each.
  */
 
-function fileInfo(movie: string): { name: string; id: string; ext: string } {
+type FileInfo = {
+  name: string;
+  id: string;
+  ext: string;
+};
+
+function fileInfo(movie: string): FileInfo {
   return {
     name: movie.split(`[`)[0]?.trim(),
     id: movie.split(`[`)[1]?.split(`].`)[0]?.trim(),
@@ -19,7 +25,7 @@ function fileInfo(movie: string): { name: string; id: string; ext: string } {
   };
 }
 
-function getNewMovies() {
+function getNewMovies(): Promise<FileInfo[]> {
   return new Promise((res, rej) => {
     fs.readdir(process.env.MOVIES_DIR ?? `nothing`, (filesErr, files) =>
       fs.readdir(process.env.MOVIES_JSON ?? `nothing`, (jsonErr, json) => {
@@ -33,9 +39,11 @@ function getNewMovies() {
         const jsonMap = new Map();
         if (Array.isArray(json))
           json.forEach((jsonFile) => jsonMap.set(jsonFile, ``));
-        const newMovies = files.reduce((all: string[], current: string) => {
-          const { id } = fileInfo(current);
-          return jsonMap.has(`.[${id}].json`) || !id ? all : [id, ...all];
+        const newMovies = files.reduce((all: FileInfo[], current: string) => {
+          const file = fileInfo(current);
+          return jsonMap.has(`.[${file.id}].json`) || !file.id
+            ? all
+            : [file, ...all];
         }, []);
         return res(newMovies);
       })
@@ -46,36 +54,37 @@ function getNewMovies() {
 // Temporary project approach -- Store metadata in JSON documents.
 // I should use Mongo for this, but soon I'll be moving to postgres anyways.
 
-function makeJSON(id: string): Promise<void> {
+function makeJSON({ id, name }: FileInfo): Promise<void> {
   return axios
     .get(
       `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US&append_to_response=videos`
     )
     .then(({ data }) => {
-      //   console.log(data);
-      //   const metadata = JSON.stringify({
-      //     id: data.id,
-      //     title: data.title,
-      //     backdrop: `${process.env.TMDB_IMAGES}${data.backdrop_path}`,
-      //     overview: data.overview,
-      //     path: `${process.env.MOVIES_DIR}`,
-      //     poster: `${process.env.TMDB_IMAGES}${data.poster_path}`,
-      //     genres: data.genres.map(({ name }: { name: string }) => name),
-      //     relase: new Date(data.release_date).getFullYear(),
-      //     trailers: data.videos,
-      //   });
-      //   return fs.writeFile(
-      //     `${path.resolve(__dirname)}/.Fight_Club.json`,
-      //     metadata,
-      //     () => console.log(`done`)
-      //   );
+      const metadata = JSON.stringify({
+        id: data.id,
+        title: data.title,
+        backdrop: `${process.env.TMDB_IMAGES}${data.backdrop_path}`,
+        overview: data.overview,
+        path: `${process.env.MOVIES_DIR}`,
+        poster: `${process.env.TMDB_IMAGES}${data.poster_path}`,
+        genres: data.genres.map(({ name }: { name: string }) => name),
+        relase: new Date(data.release_date).getFullYear(),
+        trailers: data.videos,
+      });
+      return fs.writeFile(
+        `${path.resolve(process.env.MOVIES_JSON ?? `./`)}/.${id}.json`,
+        metadata,
+        () =>
+          console.log(
+            `${name} is done => ${process.env.MOVIES_JSON}/.${id}.json`
+          )
+      );
     })
     .catch(console.error);
 }
 
-async function main() {
-  const newMovies = await getNewMovies();
-  console.log(newMovies);
-}
-
-main();
+getNewMovies()
+  .then((newMovies) =>
+    Promise.all(newMovies.map((newMovie) => makeJSON(newMovie)))
+  )
+  .catch(console.error);
